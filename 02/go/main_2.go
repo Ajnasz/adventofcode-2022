13c3468286bd388db322e83a34c52f77882ae72c
Lajos Koszti <ajnasz@ajnasz.hu>
2022-12-02 18:13:05 +0100

    solve 2. issue on day 2 in golang
    one of them uses goroutines to calculate concurrently

---
+++ 02/go/main_2.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const SCORE_ROCK = 1
+const SCORE_PAPER = 2
+const SCORE_SCISSOR = 3
+const SCORE_WIN = 6
+const SCORE_DRAW = 3
+const SCORE_LOOSE = 0
+
+func isRock(i string) bool {
+	return i == "A"
+}
+
+func isPaper(i string) bool {
+	return i == "B"
+}
+
+func isScissor(i string) bool {
+	return i == "C"
+}
+
+func shouldDraw(i string) bool {
+	return i == "Y"
+}
+
+func shouldWin(i string) bool {
+	return i == "Z"
+}
+
+func shouldLose(i string) bool {
+	return i == "X"
+}
+
+func getShapeScore(shape string) int {
+	if isRock(shape) {
+		return SCORE_ROCK
+	}
+
+	if isPaper(shape) {
+		return SCORE_PAPER
+	}
+
+	if isScissor(shape) {
+		return SCORE_SCISSOR
+	}
+
+	return 0
+}
+
+func getWinner(i string) string {
+	if isRock(i) {
+		return "B"
+	}
+
+	if isPaper(i) {
+		return "C"
+	}
+
+	return "A"
+}
+
+func getLoser(i string) string {
+	if isRock(i) {
+		return "C"
+	}
+
+	if isPaper(i) {
+		return "A"
+	}
+
+	return "B"
+}
+
+func getScore(other, my string) int {
+	if shouldDraw(my) {
+		return SCORE_DRAW + getShapeScore(other)
+	}
+
+	if shouldWin(my) {
+		return SCORE_WIN + getShapeScore(getWinner(other))
+	}
+
+	return SCORE_LOOSE + getShapeScore(getLoser(other))
+}
+
+func getShapes(line string) (string, string) {
+	split := strings.Split(line, " ")
+	return split[0], split[1]
+}
+
+func main() {
+	readFile, err := os.Open("../input")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer readFile.Close()
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	score := 0
+	for fileScanner.Scan() {
+		other, my := getShapes(fileScanner.Text())
+		score += getScore(other, my)
+	}
+
+	fmt.Println(score)
+}
